84dd9224aece6df3f7fdf310bf10e6b1239ab7d5
Bananek96 <Banasiak_Jakub@wp.pl>
2023-03-23 12:28:50 +0100

    Basics reminder, operating on lists

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,40 @@
-blockchain = []
+blockchain = [[1]]
 
-def add_value():
-    blockchain.append(5.3)
+
+def get_last_blockchain_value():
+    """ Return the last value of current blockchain. """
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction):
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_user_input():
+    user_input = int(input("Yout tansaction amount: "))
+    return user_input
+
+
+def print_data(name, age, decade):
+    print("Your data: ", name, ",", age, ", you live", decade, "decades.")
+
+def main(args):
+    x = int(input("How many transaction?: "))
+
+    for i in range(x):
+        tx_amount = get_user_input()
+        add_value(tx_amount, get_last_blockchain_value())
+
     print(blockchain)
 
-add_value()
+    name = input("Your name: ")
+    age = int(input("Yout age: "))
+    decade = age//10;
+
+    print_data(name, age, decade)
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
