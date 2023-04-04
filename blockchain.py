8ccfe50a817e1c77297f9b6dff52d778be838f93
Bananek96 <Banasiak_Jakub@wp.pl>
2023-04-04 16:21:31 +0200

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -3,10 +3,14 @@ blockchain = [[1]]
 
 def get_last_blockchain_value():
     """ Return the last value of current blockchain. """
+    if len(blockchain) < 1:
+        return None
     return blockchain[-1]
 
 
 def add_value(transaction_amount, last_transaction):
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
 
@@ -14,24 +18,36 @@ def get_user_input():
     user_input = int(input("Yout tansaction amount: "))
     return user_input
 
+def get_user_choice():
+    user_input = int(input('Your choice: '))
+    return user_input
 
 def print_data(name, age, decade):
     print("Your data: ", name, ",", age, ", you live", decade, "decades.")
 
 def main(args):
-    x = int(input("How many transaction?: "))
+    # x = int(input("How many transaction?: "))
+    # for i in range(x):
+    #     tx_amount = get_user_input()
+    #     add_value(tx_amount, get_last_blockchain_value())
+    # print(blockchain)
 
-    for i in range(x):
-        tx_amount = get_user_input()
-        add_value(tx_amount, get_last_blockchain_value())
+    while True:
+        print('Manu:')
+        print('1: Add new transaction value: ')
+        print('2. Print your blockchain: ')
+        print('3. Finish loop:')
+        user_choice = get_user_choice()
+        if user_choice == 1:
+            tx_amount = get_user_input()
+            add_value(tx_amount, get_last_blockchain_value())
+        elif user_choice == 2:
+            print(blockchain)
+        elif user_choice == 3:
+            break
+        else:
+            print('Wrong choice!!')
 
-    print(blockchain)
-
-    name = input("Your name: ")
-    age = int(input("Yout age: "))
-    decade = age//10;
-
-    print_data(name, age, decade)
     return 0
 
 
